8e85a823bb0815f5a014c1367c8a0d545fbeb91a
Roman Filippov <rf@romanfilippov.com>
2022-02-22 22:52:01 +0700

    chore: upgrades cdk

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,16 +1,18 @@
-import { awscdk } from "projen";
+import { awscdk } from 'projen';
+
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: "Roman Filippov",
-  authorAddress: "rf@romanfilippov.com",
-  cdkVersion: "2.1.0",
-  defaultReleaseBranch: "main",
-  name: "cdk-turborepo-remote-caching",
+  author: 'Roman Filippov',
+  authorAddress: 'rf@romanfilippov.com',
+  cdkVersion: '2.13.0',
+  defaultReleaseBranch: 'main',
+  name: 'cdk-turborepo-remote-caching',
   projenrcTs: true,
-  repositoryUrl: "https://github.com/rf/cdk-turborepo-remote-caching.git",
+  repositoryUrl: 'https://github.com/moltar/cdk-turborepo-remote-caching.git',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+
 project.synth();
