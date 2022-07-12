212556d3bb6e4abbbe6eb411969a43429b1272eb
Roman Filippov <rf@romanfilippov.com>
2022-07-12 10:28:54 +0100

    build: specify account

--- src/turborepo-remote-caching.integ.ts
+++ src/turborepo-remote-caching.integ.ts
@@ -1,7 +1,18 @@
 import { Stack, App } from 'aws-cdk-lib';
 import { TurborepoRemoteCaching } from './turborepo-remote-caching';
 
-const app = new App({ analyticsReporting: false });
-const stack = new Stack(app, TurborepoRemoteCaching.name);
+const app = new App({
+  analyticsReporting: false,
+});
+
+const stack = new Stack(
+  app,
+  TurborepoRemoteCaching.name, {
+    env: {
+      account: '708548510583',
+      region: 'eu-west-1',
+    },
+  },
+);
 
 new TurborepoRemoteCaching(stack, TurborepoRemoteCaching.name);
