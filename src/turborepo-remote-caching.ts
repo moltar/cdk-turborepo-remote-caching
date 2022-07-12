d332e899c2a64f5d1bc4c1f98972ddd2a0a3d700
Roman Filippov <rf@romanfilippov.com>
2022-07-12 10:29:04 +0100

    refactor: use cloudfront

--- src/turborepo-remote-caching.ts
+++ src/turborepo-remote-caching.ts
@@ -1,4 +1,4 @@
-import { aws_s3, aws_apigateway, Duration, aws_iam, RemovalPolicy, aws_logs } from 'aws-cdk-lib';
+import { aws_s3, Duration, RemovalPolicy, aws_cloudfront, aws_cloudfront_origins, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export class TurborepoRemoteCaching extends Construct {
@@ -16,136 +16,22 @@ export class TurborepoRemoteCaching extends Construct {
       ],
     });
 
-    const credentialsRole = new aws_iam.Role(this, 'Credentials', {
-      assumedBy: new aws_iam.ServicePrincipal('apigateway.amazonaws.com'),
-    });
+    const originAccessIdentity = new aws_cloudfront.OriginAccessIdentity(this, 'OAI');
 
-    storage.grantReadWrite(credentialsRole);
+    storage.grantReadWrite(originAccessIdentity, 'v8/artifacts/*');
 
-    const accessLog = new aws_logs.LogGroup(this, 'AccessLog', {
-      removalPolicy: RemovalPolicy.DESTROY,
-    });
-
-    const gateway = new aws_apigateway.RestApi(this, `${id}API`, {
-      binaryMediaTypes: ['*/*'],
-      description: 'API Gateway for Turborepo remote cache server.',
-      deployOptions: {
-        stageName: 'cache',
-        accessLogDestination: new aws_apigateway.LogGroupLogDestination(accessLog),
-        accessLogFormat: aws_apigateway.AccessLogFormat.jsonWithStandardFields({
-          caller: true,
-          httpMethod: true,
-          ip: true,
-          protocol: true,
-          requestTime: true,
-          resourcePath: true,
-          responseLength: true,
-          status: true,
-          user: true,
+    const distribution = new aws_cloudfront.Distribution(this, 'Distribution', {
+      defaultBehavior: {
+        origin: new aws_cloudfront_origins.S3Origin(storage, {
+          originAccessIdentity,
         }),
+        allowedMethods: aws_cloudfront.AllowedMethods.ALLOW_ALL,
       },
     });
 
-    const v8 = gateway.root.addResource('v8');
-
-    const resource = v8
-      .addResource('artifacts')
-      .addResource('{hash}');
-
-    resource.addMethod(
-      'PUT',
-
-      new aws_apigateway.AwsIntegration({
-        service: 's3',
-        integrationHttpMethod: 'PUT',
-        path: `${storage.bucketName}/{slug}/{hash}`,
-        options: {
-          credentialsRole,
-          integrationResponses: [
-            {
-              statusCode: '200',
-            },
-          ],
-          requestParameters: {
-            'integration.request.path.slug': 'method.request.querystring.slug',
-            'integration.request.path.hash': 'method.request.path.hash',
-          },
-        },
-      }),
-
-      {
-        methodResponses: [
-          {
-            statusCode: '200',
-            responseParameters: {
-              'method.response.header.Content-Type': true,
-            },
-          },
-        ],
-        requestParameters: {
-          'method.request.querystring.slug': true,
-          'method.request.path.hash': true,
-          'method.request.header.Content-Type': true,
-        },
-      });
-
-    resource.addMethod(
-      'GET',
-
-      new aws_apigateway.AwsIntegration({
-        service: 's3',
-        integrationHttpMethod: 'GET',
-        path: `${storage.bucketName}/{slug}/{hash}`,
-        options: {
-          credentialsRole,
-          integrationResponses: [
-            {
-              statusCode: '200',
-            },
-          ],
-          requestParameters: {
-            'integration.request.path.slug': 'method.request.querystring.slug',
-            'integration.request.path.hash': 'method.request.path.hash',
-          },
-        },
-      }),
-
-      {
-        methodResponses: [
-          {
-            statusCode: '200',
-            responseParameters: {
-              'method.response.header.Content-Type': true,
-            },
-          },
-        ],
-        requestParameters: {
-          'method.request.querystring.slug': true,
-          'method.request.path.hash': true,
-          'method.request.header.Content-Type': true,
-        },
-      });
-
-    /**
-     * The following endpoints are just stubs, and turbo isn't calling them during runs
-     */
-
-    const v2 = gateway.root.addResource('v2');
-
-    v2
-      .addResource('user')
-      .addMethod('GET', new aws_apigateway.MockIntegration({
-        integrationResponses: [{
-          statusCode: '200',
-        }],
-      }));
-
-    v2
-      .addResource('teams')
-      .addMethod('GET', new aws_apigateway.MockIntegration({
-        integrationResponses: [{
-          statusCode: '200',
-        }],
-      }));
+    new CfnOutput(this, 'DistributionURL', {
+      description: 'Distribution URL.',
+      value: `https://${distribution.domainName}`,
+    });
   }
 }
